a299dccb3923fce31269cc64e1f692897f43fd90
Zack Ng <ngzhicai98@gmail.com>
2022-11-09 14:19:58 +0800

    fix(MapNode): Fix copying and moving

--- src/MapNode.hxx
+++ src/MapNode.hxx
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <type_traits>
+
 namespace nhzaci {
 
 /**
@@ -8,17 +10,49 @@ namespace nhzaci {
  * @tparam Key  Type of key objects, must be default contructible
  * @tparam T    Type of mapped objects
  */
-template <typename Key, typename T> struct MapNode {
+template <typename Key, typename T, typename Deleter = std::default_delete<T>>
+struct MapNode {
+  static_assert(std::is_default_constructible_v<Key>,
+                "Key of a MapNode must be default constructible");
+  static_assert(std::is_default_constructible_v<T>,
+                "Mapped object T of a MapNode must be default constructible");
+
   // TODO: Add template concept for Key to be default constructible
   MapNode() : key{}, t_p{nullptr} {};
+
+  // used in operator[] of hash map, default construct with some key
+  // and default construction of T
+  MapNode(Key k) : key{k}, t_p{new T()} {};
+
   MapNode(Key k, T *t) : key{k}, t_p{t} {};
+
+  MapNode(const MapNode &other) {
+    key = other.key;
+    t_p = new T(*other.t_p);
+  }
+  MapNode &operator=(const MapNode &other) {
+    key = other.key;
+    t_p = new T(*other.t_p);
+    return *this;
+  }
+
+  MapNode(MapNode &&other) : key{other.key}, t_p{other.t_p} {
+    other.t_p = nullptr;
+  }
+  MapNode &operator=(MapNode &&other) {
+    key = other.key;
+    t_p = other.t_p;
+
+    other.t_p = nullptr;
+    return *this;
+  }
+
   Key key;
   T *t_p;
 
   ~MapNode() {
-    // TODO: Use alloc to deallocate
     if (t_p != nullptr)
-      delete t_p;
+      Deleter()(t_p);
   }
 
   friend inline std::ostream &operator<<(std::ostream &os,
